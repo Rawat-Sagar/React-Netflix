9bfb3f3620e3d4eb5f87b54e95b44b72350fbca9
Sagar Rawat <sagarrawat089@gmail.com>
2021-03-12 22:39:13 +0530

    Delete Impexp.jsx

--- src/Impexp.jsx
+++
@@ -1,9 +0,0 @@
-const food = "Puran Poli";
-const streetfood = "Vada Pav";
-
-function myPlace(){
-    let place = "Mumbai";
-    return place;
-}
-export default food;
-export  {streetfood,myPlace};
