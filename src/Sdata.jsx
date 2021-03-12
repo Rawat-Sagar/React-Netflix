b4af48c81d71fcd0b274c91eaa9b82bf706e3986
Sagar Rawat <sagarrawat089@gmail.com>
2021-03-12 19:38:11 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,22 @@
+const Sdata = [
+    {
+        imgsrc :"https://www.tjtoday.org/wp-content/uploads/2018/02/c245fb206fecea20e4f18e26dc8fa74aae6f80b5.jpg" ,
+        title :"A Netflix Original Series" ,
+        sname :"DARK" ,
+        link :"https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc :"https://i.pinimg.com/originals/c3/0d/2b/c30d2b50f80988b4fe5dee93b0967aa3.jpg",
+        title :"A Netflix Original Series",
+        sname :"PEAKY BLINDERS",
+        link :"https://www.netflix.com/in/title/80002479",
+    },
+    {
+        imgsrc :"https://images-na.ssl-images-amazon.com/images/M/MV5BMjEzMDAxOTUyMV5BMl5BanBnXkFtZTgwNzAxMzYzOTE@._V1_.jpg",
+        title :"A Netflix Original Series",
+        sname :"STRANGERS THINGS",
+        link :"https://www.netflix.com/in/title/80057281",
+    },
+];
+
+export default Sdata;
---
+++ src/index.js
@@ -0,0 +1,56 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards'
+import Sdata from './Sdata'
+import './index.css'
+import reportWebVitals from './reportWebVitals';
+
+
+// normal function
+// function netflixcard(val)
+// {
+//     return(
+//          <Card 
+//         imgsrc={val.imgsrc} 
+//         title={val.title} 
+//         sname={val.sname} 
+//         link={val.link}    
+//         />
+//     );
+// }
+
+ReactDOM.render(
+    <>
+        <h1 className="heading">List of top 3 Netflix Series</h1>
+        {/* {Sdata.map(netflixcard)} */}
+
+        {/* Fat arrow */}
+        {Sdata.map((val) =>{
+            return(
+                <Card 
+                imgsrc={val.imgsrc} 
+                title={val.title} 
+                sname={val.sname} 
+                link={val.link}
+
+                />
+            );
+        })}
+    </>,
+    document.getElementById('root')
+);
+
+
+
+// git init
+// git add -A
+// git commit -m 'Added my project'
+// git remote add origin git@github.com:sammy/my-new-project.git
+// git push -u -f origin master
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
